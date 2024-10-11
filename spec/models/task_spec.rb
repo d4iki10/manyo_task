d592b83dfc26a99ff97c5c4dd57070fae0cc8bc0
Daiki <daqidiye@gmail.com>
2024-10-11 18:27:30 +0900

    課題提出_v1

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.create(title: '', content: '企画書を作成する。')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクの説明が空文字の場合' do
+      it 'バリデーションに失敗する' do
+      end
+    end
+
+    context 'タスクのタイトルと説明に値が入っている場合' do
+      it 'タスクを登録できる' do
+      end
+    end
+  end
+end
