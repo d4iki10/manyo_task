68131fa645efceccbeea7c4fcb0e7d5e848b1b29
Daiki <daqidiye@gmail.com>
2024-10-20 23:22:46 +0900

    step4エラーメッセージ確認用_v2

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,7 +13,7 @@ module Admin
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to admin_users_path, notice: I18n.t('flash.create_success', model: I18n.t('activerecord.models.user'))
+        redirect_to admin_users_path, notice: I18n.t('flash.user_created', model: I18n.t('activerecord.models.user'))
       else
         render :new
       end
@@ -32,7 +32,7 @@ module Admin
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-        redirect_to admin_users_path, notice: t('flash.update_success', model: t('activerecord.models.user'))
+        redirect_to admin_users_path, notice: t('flash.user_updated', model: t('activerecord.models.user'))
       else
         render :edit
       end
@@ -44,7 +44,7 @@ module Admin
         redirect_to admin_users_path, alert: t('flash.delete_last_admin')
       else
         @user.destroy
-        redirect_to admin_users_path, notice: t('flash.destroy_success', model: t('activerecord.models.user'))
+        redirect_to admin_users_path, notice: t('flash.user_destroyed', model: t('activerecord.models.user'))
       end
     end
 
@@ -56,7 +56,7 @@ module Admin
 
     def admin_user
       unless current_user&.admin?
-        flash[:alert] = t('flash.alert.admin_access')
+        flash[:alert] = t('alert.admin_access')
         redirect_to tasks_path
       end
     end
